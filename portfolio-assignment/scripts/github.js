89b82b4b7aad88cccdb9a9f122eeb574660dde1b
Amy Leek <amyeleek@gmail.com>
2016-01-19 18:58:40 -0800

    More merging

--- portfolio-assignment/scripts/github.js
+++ portfolio-assignment/scripts/github.js
@@ -7,17 +7,33 @@
   github.getData = function(type, callback){
     var qs = '?per_page=10&sort=updated';
 
-    $.ajax({
-      url: 'https://api.github.com/users/MarsDragon/' + type + qs,
-      method: 'GET',
-      ifModified: true,
-      header: {'Authorization': 'token ' + gitAuth.getToken()},
+    var etag = '';
+    //start off by grabbing etag to check if JSON has been changed
+    $.ajax('https://api.github.com/users/MarsDragon/' + type + qs, {
+      method: 'HEAD',
       success: function(data, msg, xhr){
-        github.all = data;
-        localStorage[type] = JSON.stringify(data);
+        etag = xhr.getResponseHeader('eTag');
+
+        //once we have that, check if it matches what's in local storage
+        if(localStorage.gitEtag == etag){
+          //if yes, just use that
+          github.all = JSON.parse(localStorage[type]);
+        }else{
+          $.ajax({
+            url: 'https://api.github.com/users/MarsDragon/' + type + qs,
+            method: 'GET',
+            ifModified: true,
+            header: {'Authorization': 'token ' + gitAuth.getToken()},
+            success: function(data, msg, xhr){
+              github.all = data;
+              localStorage[type] = JSON.stringify(data);
+              localStorage.setItem('gitEtag', etag);
+            }
+          }).done(function(){
+            callback(type);
+          });
+        }
       }
-    }).done(function(){
-      callback(type);
     });
   };
 
